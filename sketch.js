27e61d7e51e8a9bf21a1b12b6f3c8287ac4fa722
Nititorn2006 <nititornwongsuwan2006@gmail.com>
2025-09-19 21:45:06 +0700

    game title

--- sketch.js
+++ sketch.js
@@ -8,21 +8,30 @@ function setup() {
     startButton.position(windowWidth / 2, windowHeight / 2);
     startButton.mouseClicked(gameStarted);
     startButton.class('start-button');
+    title = select('.gameTitle');
 }
 
 function gameStarted() {
     gameStarted = true;
     startButton.hide();
+    title.hide();
 }
 
-// function drawSquare() {
-//     color(255);
-//     line(windowWidth / 2 - 50, windowHeight / 2 - 50, windowWidth / 2 + 50, windowHeight / 2 + 50);
-// }
+function drawSquare(x, y) {
+    stroke(255);
+    line(windowWidth / 2 - 50, windowHeight / 2 - 50, windowWidth / 2 - 50, windowHeight / 2 + 50);
+    line(windowWidth / 2 - 50, windowHeight / 2 - 50, windowWidth / 2 + 50, windowHeight / 2 - 50);
+    line(windowWidth / 2 - 50, windowHeight / 2 + 50, windowWidth / 2 + 50, windowHeight / 2 + 50);
+    line(windowWidth / 2 + 50, windowHeight / 2 - 50, windowWidth / 2 + 50, windowHeight / 2 + 50);
+}
+
+function drawBox() {
+    drawSquare();
+}
 
 function draw() {
     if (gameStarted == true) {
         background(1);
-        // drawSquare();
+        drawSquare(100, 100 + 100);
     }
 }
