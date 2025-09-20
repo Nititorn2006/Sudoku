751817f7b717e7ba7f901f2ae1551dddb7cae9c0
Nititorn2006 <nititornwongsuwan2006@gmail.com>
2025-09-20 21:22:46 +0700

    Draw box

--- sketch.js
+++ sketch.js
@@ -19,19 +19,45 @@ function gameStarted() {
 
 function drawSquare(x, y) {
     stroke(255);
-    line(windowWidth / 2 - 50, windowHeight / 2 - 50, windowWidth / 2 - 50, windowHeight / 2 + 50);
+    stroke(255);
+    noFill();
+    rectMode(CENTER);
+    rect(x, y, 100, 100);
     line(windowWidth / 2 - 50, windowHeight / 2 - 50, windowWidth / 2 + 50, windowHeight / 2 - 50);
-    line(windowWidth / 2 - 50, windowHeight / 2 + 50, windowWidth / 2 + 50, windowHeight / 2 + 50);
     line(windowWidth / 2 + 50, windowHeight / 2 - 50, windowWidth / 2 + 50, windowHeight / 2 + 50);
+    line(windowWidth / 2 + 50, windowHeight / 2 + 50, windowWidth / 2 - 50, windowHeight / 2 + 50);
+    line(windowWidth / 2 - 50, windowHeight / 2 + 50, windowWidth / 2 - 50, windowHeight / 2 - 50);
 }
 
-function drawBox() {
-    drawSquare();
+function drawBox(x, y) {
+    stroke(255);
+    strokeWeight(8);
+    rect(windowWidth / 2, windowHeight / 2, 300, 300);
+    strokeWeight(4);
+    noFill();
+    rectMode(CENTER);
+    drawSquare(windowWidth / 2, windowHeight / 2);
+    drawSquare(windowWidth / 2 - 100, windowHeight / 2 - 100);
+    drawSquare(windowWidth / 2, windowHeight / 2 - 100);
+    drawSquare(windowWidth / 2 + 100, windowHeight / 2 - 100);
+    drawSquare(windowWidth / 2 - 100, windowHeight / 2);
+    drawSquare(windowWidth / 2 + 100, windowHeight / 2);
+    drawSquare(windowWidth / 2 - 100, windowHeight / 2 + 100);
+    drawSquare(windowWidth / 2, windowHeight / 2 + 100);
+    drawSquare(windowWidth / 2 + 100, windowHeight / 2 + 100);
+    strokeWeight(1);
+}
+
+function drawGrid(x, y) {
+    stroke(255);
+    noFill();
+    rectMode(CENTER);
+    drawBox(windowWidth / 2, windowHeight / 2);
 }
 
 function draw() {
     if (gameStarted == true) {
         background(1);
-        drawSquare(100, 100 + 100);
+        drawGrid(windowWidth / 2, windowHeight / 2);
     }
 }
